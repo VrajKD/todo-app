25466cd6c3031384aa0a9f30c8d7738dc5d6058f
Vraj Dugar <vrajkishan.dugar2017@vitstudent.ac.in>
2019-05-22 16:53:32 +0530

    Setting up folder strucutre

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
- 
+ var express = require('express');
+ var app = express();
+ var bodyParser = require('body-parser')
+ var mongoose = require('mongoose')
+ var User = require('./models/users.js')
+ var List = require('./models/lists.js')
+ var passport = require('passport');
+ var passportLocal = require('passport-local');
+ var methodOverride = require('method-override');
+ var expressSession = require('express-session');
+ app.set('view engine', 'ejs')
+ app.use(bodyParser.urlencoded({
+     extended: true
+ }));
+ app.use(methodOverride('_method'))
+ app.use(express.static(__dirname + '/public'));
--- models/lists.js
+++ models/lists.js
@@ -1,12 +1,34 @@
-var mongoose=require("mongoose");
+var mongoose = require("mongoose");
 
-var listSchema= new mongoose.Schema({
-    title:{type:String,required:true},
-    created:{type:Date,default:Date.now},
-    description:String,
-    todo:{
-        id:mongoose.Schema.Types.ObjectId,
-        ref:"Todo"
+var listSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
     },
-    color:"String"
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    description: String,
+    todos: [{
+        description: {
+            type: String,
+            required: true
+        },
+        date: Date
+    }],
+    color: "String",
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: "String"
+    },
+    edited: {
+        type: Date,
+        default: Date.now
+    }
 })
+
+module.exports - mongoose.model("List", listSchema);
